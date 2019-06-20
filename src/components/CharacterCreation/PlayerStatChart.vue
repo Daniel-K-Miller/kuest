a2bf40b12647c916ec7ef3f10339ef0425cec0df
Daniel Kieron Miller <daniel_k_miller@hotmail.com>
2019-06-20 23:53:30 +0100

    Corrected d3 data change within the watcher in PlayerStatChart.vue. Need to carry on implementing new features for this component.

--- src/components/CharacterCreation/PlayerStatChart.vue
+++ src/components/CharacterCreation/PlayerStatChart.vue
@@ -39,37 +39,39 @@ export default {
       .append("rect")
       .attr("width", 75)
       .attr("height", (d, i) => {
-        return d;
+        return d * 3;
       })
       .attr("x", (d, i) => {
         return i * 100;
+      })
+      .attr("y", (d, i) => {
+        return h - d * 3;
       })
       .attr("fill", "red");
   },
   watch: {
     playerDataSpeciesUpdate() {
       let h = 200;
-      d3.select("#test")
-        .select("svg")
-        .append("svg")
-        .attr("width", 500)
-        .attr("height", h)
+      d3.select("svg")
         .selectAll("rect")
         .data([
           this.playerData.attributes.species.brute,
           this.playerData.attributes.species.resistance,
           this.playerData.attributes.species.life
         ])
-        .enter()
-        .append("rect")
         .attr("width", 75)
         .attr("height", (d, i) => {
-          return d;
+          return d * 3;
         })
         .attr("x", (d, i) => {
           return i * 100;
         })
-        .attr("fill", "red");
+        .attr("y", (d, i) => {
+          return h - d * 3;
+        })
+        .attr("fill", (d, i) => {
+          return i === 0 ? `blue` : i === 1 ? `green` : `red`;
+        });
     }
   }
 };
